c3b7b76157689b76be94575b8cdeef276f72a2a9
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-10-19 14:33:29 +0600

    Single product routes set and load data to database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3300;
 
@@ -32,6 +32,13 @@ async function run() {
     app.post("/products", async (req, res) => {
       const products = req.body;
       const result = await productsDB.insertOne(products);
+      res.send(result);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsDB.findOne(query);
       res.send(result);
     });
 
